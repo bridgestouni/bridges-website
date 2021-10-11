1328f237cf323b790db229a91d35b5a82eed90a4
Varun Garg <varungarg0826@gmail.com>
2021-10-10 22:24:25 -0400

    update port

--- server/app.js
+++ server/app.js
@@ -57,4 +57,8 @@ app.post("/newsletter-subscribe", (req, res) => {
     });
 });
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+//app.listen(port, () => console.log(`Server running on port ${port}`));
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
